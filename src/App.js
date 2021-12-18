9c0bfe13c54a47384472a31ca34ee2fa38e02bd1
belalE <belalelsiesy@gmail.com>
2021-12-18 15:15:20 -0600

    remove styled components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import "./App.css";
 import Person from "./Person/Person";
 
-const StyledButton = styled.button`
-  background-color: ${(props) => (props.showing ? "red" : "green")};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  &:hover {
-    background-color: ${(props) => (props.showing ? "salmon" : "lightgreen")};
-    color: black;
-  }
-`;
 
 class App extends Component {
   state = {
@@ -90,12 +77,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <StyledButton
-          showing={this.state.showPersons}
-          onClick={this.togglePersonsHandler}
+        <button className="button" onClick={this.togglePersonsHandler}
         >
           Toggle Persons
-        </StyledButton>
+        </button>
         {persons}
       </div>
     );
